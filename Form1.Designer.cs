9b1ab2e95788abdd81516905d2d6beff46e9a563
Tim Graupmann <tgraupmann@gmail.com>
2024-01-23 18:15:43 -0800

    Fixed typo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -197,7 +197,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(61, 32);
             this.label1.TabIndex = 3;
-            this.label1.Text = "CNTL+ ENTER";
+            this.label1.Text = "CTRL+ ENTER";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // DropDownFocus
