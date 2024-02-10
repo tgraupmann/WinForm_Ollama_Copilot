7c41dac62e75e9cb2479f28275855b0bb83761b2
Tim Graupmann <tgraupmann@gmail.com>
2024-02-10 13:50:39 -0800

    Added server API

--- TesseractOCRServer.py
+++ TesseractOCRServer.py
@@ -1,22 +1,56 @@
+# install dependencies:
+# pip3 install uvicorn
+# pip3 install FastAPI[all]
+# pip install pillow
+# pip install pytesseract
+
 # Install Tesseract-OCR - https://tesseract-ocr.github.io/tessdoc/Installation.html
 # Add Tesseract to your path: C:\Program Files\Tesseract-OCR
-# pip install pytesseract
-# pip install pillow
 
+# launch the app
+# python3 -m uvicorn TesseractOCRServer:app --reload --port 11439 --log-level error
+# browse: http://127.0.0.1:11439
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 import pytesseract
 from PIL import Image
+import base64
+import io
 
-# Set the path to the image file
-image_path = "images/image_8.png"
+app = FastAPI()
 
-# Load the image using Pillow
-img = Image.open(image_path)
+class ImageBytes(BaseModel):
+  data: str
 
-# Set the language for OCR (english by default)
-lang = "eng"
+@app.post('/speak', response_class=HTMLResponse)
+async def image_to_string(item:ImageBytes):
 
-# Use Tesseract-OCR to recognize text in the image
-text = pytesseract.image_to_string(img, lang=lang)
+    # Load the image using Pillow
+    # Set the base64 string of the PNG image
+    base64_string = item.data
 
-# Print the recognized text
-print(text)
+    print(f"image_to_string: base64_string={base64_string}")
+
+    # Decode the base64 string into bytes
+    image_bytes = base64.b64decode(base64_string)
+
+    # Create a BytesIO object to load the image
+    image_stream = io.BytesIO(image_bytes)
+
+    # Open the image using Pillow
+    img = Image.open(image_stream)
+
+    # Set the language for OCR (english by default)
+    lang = "eng"
+
+    # Use Tesseract-OCR to recognize text in the image
+    text = pytesseract.image_to_string(img, lang=lang)
+
+    # Print the recognized text
+    print(text)
+
+    return { "result": text }
+
+print ("Python Tesseract-OCR Server Started")
